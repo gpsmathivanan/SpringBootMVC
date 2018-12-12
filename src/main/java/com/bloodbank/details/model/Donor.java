d032b04414a1586cc3d3e41dd55e96637adb46ac
gpsmathivanan <45365875+gpsmathivanan@users.noreply.github.com>
2018-12-13 00:52:44 +0530

    Add files via upload
    Initial Commit

---
+++ src/main/java/com/bloodbank/details/model/Donor.java
@@ -0,0 +1,36 @@
+package com.bloodbank.details.model;
+
+public class Donor {
+	private String name;
+	private Integer age;
+	private String street;
+	private String postcode;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getPostcode() {
+		return postcode;
+	}
+	public void setPostcode(String postcode) {
+		this.postcode = postcode;
+	}
+	
+	
+	
+
+}
