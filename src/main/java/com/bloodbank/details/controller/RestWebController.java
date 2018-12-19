cf37a814b09753f0d003cd7d67f7b5c76ae8e858
gpsmathivanan <45365875+gpsmathivanan@users.noreply.github.com>
2018-12-20 00:17:58 +0530

    Add files via upload
    Final Changes

--- src/main/java/com/bloodbank/details/controller/RestWebController.java
+++ src/main/java/com/bloodbank/details/controller/RestWebController.java
@@ -1,13 +1,15 @@
 package com.bloodbank.details.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bloodbank.details.dao.BloodBankDAOImpl;
+import com.bloodbank.details.model.Admin;
 import com.bloodbank.details.model.Donor;
+import com.bloodbank.details.model.Seeker;
 
 @RestController
 @RequestMapping("/api/bloodbank")
@@ -16,10 +18,22 @@ public class RestWebController {
 	@Autowired
 	BloodBankDAOImpl bloodBankDAOImpl;
 	
-	@PostMapping(value="/donor")
-	public String postCustomer(@RequestBody Donor donor){
-		
+	@RequestMapping(value="/donor",method=RequestMethod.POST)
+	public String saveDonorDetails(@RequestBody Donor donor){
 		bloodBankDAOImpl.saveDonorDetails(donor);
 		return "Post Successfully!";
+	}
+	
+	@RequestMapping(value="/seeker",method=RequestMethod.POST)
+	public String saveSeekerDetails(@RequestBody Seeker seeker){
+		bloodBankDAOImpl.saveSeekerDetails(seeker);
+		return "Post Successfully!";
+	}
+	
+	
+	@RequestMapping(value="/admin",method=RequestMethod.POST)
+	public String saveAdminDetails(@RequestBody Admin admin){
+		String response=bloodBankDAOImpl.saveAdminDetails(admin);
+		return response;
 	}
 }
---
+++ src/main/java/com/bloodbank/details/model/Admin.java
@@ -0,0 +1,15 @@
+package com.bloodbank.details.model;
+
+public class Admin {
+	private String bloodType;
+
+	public String getBloodType() {
+		return bloodType;
+	}
+
+	public void setBloodType(String bloodType) {
+		this.bloodType = bloodType;
+	}
+	
+	
+}
